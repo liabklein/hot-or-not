3066543643424065a3b40d7d5e685d636e7a5285
liabklein <93094739+liabklein@users.noreply.github.com>
2025-04-29 07:23:31 -0400

    register service worker from currend directory

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let tempChart = null; // To hold the chart instance
 // --- PWA Service Worker Registration ---
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(registration => {
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             })
